7f43c8cd252c652375bb93e254fa01430669d201
Emerson Kelly <123923271+Emerson-Kelly@users.noreply.github.com>
2024-02-07 18:59:22 -0500

    Get the screen and key elements

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+// Get the elements
+const screen = document.getElementById('screen');
+const keys = document.querySelectorAll('.calculator-key');
+
+});
