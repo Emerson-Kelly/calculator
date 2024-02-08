aa3841f41d9c45b32c5743814c84842f9fdf40e4
Emerson Kelly <123923271+Emerson-Kelly@users.noreply.github.com>
2024-02-07 19:34:13 -0500

    Add Script That Handles Key Clicks for Numbers 0 Through 9 as Well as Clearing the Display

--- script.js
+++ script.js
@@ -18,6 +18,48 @@ function updateDisplay() {
 }
 
 
+ // Handle digit input
+ function inputDigit(digit) {
+    const { displayValue, waitingForSecondOperand } = calculator;
+    
+    if (waitingForSecondOperand) {
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false;
+    } else {
+        calculator.displayValue =
+            displayValue === '0' ? digit : displayValue + digit;    
+    }
+}
+
+ // Handle key clicks
+ keys.forEach((key) => {
+    key.addEventListener('click', () => {
+        const keyValue = key.textContent;
+
+        if (/\d/.test(keyValue)) {
+            inputDigit(keyValue);
+            updateDisplay();
+            console.table(calculator); // CALCULATOR OBJECT TEST
+        } else if (/\+|-|\*|\//.test(keyValue)) {
+            inputOperator(keyValue);
+            console.table(calculator); // CALCULATOR OBJECT TEST
+        } else if (keyValue === '=') {
+            inputOperator('=');
+            updateDisplay();
+            console.table(calculator); // CALCULATOR OBJECT TEST
+        } else if (keyValue === 'AC') {
+            calculator.displayValue = '0';
+            calculator.firstOperand = null;
+            calculator.operator = null;
+            calculator.waitingForSecondOperand = false;
+            updateDisplay();
+            console.table(calculator); // CALCULATOR OBJECT TEST
+        }
+    });
+    
+});
+
+
 
 
 });
